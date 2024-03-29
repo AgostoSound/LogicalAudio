23f2f1045bc9cbfb9f49e88e928956d63dc318cb
AgostoSound <agostosound@gmail.com>
2024-03-29 02:21:48 -0300

    avances rantics configs

--- src/L-Rantics.cpp
+++ src/L-Rantics.cpp
@@ -7,17 +7,15 @@
 #include <iostream>
 #include <cmath>
 
-
-
 struct L_Rantics : Module {
 	enum ParamId {
 		BEAT_FRAC_PARAM,
 		L_SPREAD_PARAM,
 		R_SPREAD_PARAM,
+		SELECT_PARAM,
 		PARAMS_LEN
 	};
 	enum InputId {
-		SELECT_INPUT,
 		CLOCK_INPUT,
 		BPM_INPUT,
 		L_CV_INPUT,
@@ -35,10 +33,10 @@ struct L_Rantics : Module {
 
 	L_Rantics() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(BEAT_FRAC_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(BEAT_FRAC_PARAM, -16.f, 16.f, 1.f, "Beat Fraction");
 		configParam(L_SPREAD_PARAM, 0.f, 1.f, 0.f, "");
 		configParam(R_SPREAD_PARAM, 0.f, 1.f, 0.f, "");
-		configInput(SELECT_INPUT, "");
+		configParam(SELECT_PARAM, 0.f, 2.f, 1.f, "Beat selector");
 		configInput(CLOCK_INPUT, "");
 		configInput(BPM_INPUT, "");
 		configInput(L_CV_INPUT, "");
@@ -62,18 +60,19 @@ struct L_RanticsWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(30.504, 38.197)), module, L_Rantics::BEAT_FRAC_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(13.571, 62.671)), module, L_Rantics::L_SPREAD_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(48.496, 62.671)), module, L_Rantics::R_SPREAD_PARAM));
+		addParam(createParamCentered<BefacoTinyKnob>(mm2px(Vec(30.504, 49.33)), module, L_Rantics::BEAT_FRAC_PARAM));
+		addParam(createParamCentered<BefacoTinyKnob>(mm2px(Vec(13.571, 73.8)), module, L_Rantics::L_SPREAD_PARAM));
+		addParam(createParamCentered<BefacoTinyKnob>(mm2px(Vec(48.496, 73.8)), module, L_Rantics::R_SPREAD_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(30.531, 24.446)), module, L_Rantics::SELECT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(12.471, 24.71)), module, L_Rantics::CLOCK_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.391, 24.578)), module, L_Rantics::BPM_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(13.466, 76.172)), module, L_Rantics::L_CV_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.258, 76.039)), module, L_Rantics::R_CV_INPUT));
+        addParam(createParam<CKSSThreeHorizontal>(mm2px(Vec(25.40, 33.30)), module, L_Rantics::SELECT_PARAM));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(13.273, 95.158)), module, L_Rantics::OUT1_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(48.383, 95.174)), module, L_Rantics::OUT2_OUTPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(12.471, 35.84)), module, L_Rantics::CLOCK_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.391, 35.84)), module, L_Rantics::BPM_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(13.466, 87.3)), module, L_Rantics::L_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.258, 87.3)), module, L_Rantics::R_CV_INPUT));
+
+		addOutput(createOutputCentered<PJ3410Port>(mm2px(Vec(13.273, 106.29)), module, L_Rantics::OUT1_OUTPUT));
+		addOutput(createOutputCentered<PJ3410Port>(mm2px(Vec(48.383, 106.29)), module, L_Rantics::OUT2_OUTPUT));
 	}
 };
 
