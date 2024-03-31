b6c189bec6bc1aae6d4648b2298d58876b955cdf
AgostoSound <agostosound@gmail.com>
2024-03-31 16:24:01 -0300

    Last test commit - into develope.

--- src/L-Rantics.cpp
+++ src/L-Rantics.cpp
@@ -33,23 +33,25 @@ struct L_Rantics : Module {
 
 	// Definir los valores permitidos
 	std::vector<std::__cxx11::basic_string<char>> fractions_labels = {"÷16", "÷8", "÷4", "÷2", "0", "x2", "x4", "x8", "x16"};
+	std::vector<std::__cxx11::basic_string<char>> selector_labels = {"Clock", "???", "BPM"};
 	std::vector<float> original_fraction_values = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 	std::vector<float> normalized_fraction_values = {-16, -8, -4, -2, 0, 2, 4, 8, 16};
 
 	L_Rantics() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configSwitch(BEAT_FRAC_PARAM, 0.f, 8.f, 4.f, "Beat Fraction", fractions_labels);
-		configParam(L_SPREAD_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(R_SPREAD_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(SELECT_PARAM, 0.f, 2.f, 1.f, "Beat selector");
-		configInput(CLOCK_INPUT, "");
-		configInput(BPM_INPUT, "");
-		configInput(L_CV_INPUT, "");
-		configInput(R_CV_INPUT, "");
-		configOutput(OUT1_OUTPUT, "");
-		configOutput(OUT2_OUTPUT, "");
+		configSwitch(SELECT_PARAM, 0.f, 2.f, 0.f, "Beat selector", selector_labels);
+		configParam(L_SPREAD_PARAM, 0.f, 1.f, 0.f, "L Spread");
+		configParam(R_SPREAD_PARAM, 0.f, 1.f, 0.f, "R Spread");
+		configInput(CLOCK_INPUT, "Clock");
+		configInput(BPM_INPUT, "BPM Signal");
+		configInput(L_CV_INPUT, "L CV Spread");
+		configInput(R_CV_INPUT, "R CV Spread");
+		configOutput(OUT1_OUTPUT, "L Random");
+		configOutput(OUT2_OUTPUT, "R Random");
 	}
 
+	// Beat fraction normalizer.
 	float normalizeBeatFraction(float value) {
 		auto it = std::find(original_fraction_values.begin(), original_fraction_values.end(), value);
 		size_t index = std::distance(original_fraction_values.begin(), it);
@@ -58,8 +60,9 @@ struct L_Rantics : Module {
 
 
 	void process(const ProcessArgs& args) override {
-		float beat_fraction = params[BEAT_FRAC_PARAM].getValue();
-		beat_fraction = normalizeBeatFraction(beat_fraction);
+		float beat_fraction = params[BEAT_FRAC_PARAM].getValue();  // Beat fraction param (0-8).
+		beat_fraction = normalizeBeatFraction(beat_fraction);  // Normalidez beat fraction. (-16 +16)
+		
 		outputs[OUT1_OUTPUT].setVoltage(beat_fraction);  // Set voltage.
 	}
 };
