c5d9936f2dd4816c8d38a3a27d895293330442a8
AgostoSound <agostosound@gmail.com>
2025-02-04 23:22:35 +0100

    Add labels

--- src/L-Rantics.cpp
+++ src/L-Rantics.cpp
@@ -1,17 +1,25 @@
 #include "plugin.hpp"
+#include "random.hpp"
+#include <random>
+#include <chrono>
+#include <array>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include "componentlibrary.hpp"
 
 
 struct L_Rantics : Module {
 	enum ParamId {
 		L_SPREAD_PARAM,
 		R_SPREAD_PARAM,
+		POLARITY_SWITCH,
+		TIC_SELECTOR_SWITCH,
 		PARAMS_LEN
 	};
 	enum InputId {
-		SELECT_INPUT,
-		CLOCK_INPUT,
-		BPM_INPUT,
-		BEAT_FRAC_INPUT,
+		TIC1_INPUT,
+		TIC2_INPUT,
 		L_CV_INPUT,
 		R_CV_INPUT,
 		INPUTS_LEN
@@ -27,22 +35,20 @@ struct L_Rantics : Module {
 
 	L_Rantics() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(L_SPREAD_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(R_SPREAD_PARAM, 0.f, 1.f, 0.f, "");
-		configInput(SELECT_INPUT, "");
-		configInput(CLOCK_INPUT, "");
-		configInput(BPM_INPUT, "");
-		configInput(BEAT_FRAC_INPUT, "");
-		configInput(L_CV_INPUT, "");
-		configInput(R_CV_INPUT, "");
-		configOutput(OUT1_OUTPUT, "");
-		configOutput(OUT2_OUTPUT, "");
-	}
+		configParam(L_SPREAD_PARAM, 0.f, 1.f, 0.f, "LvL L");
+		configParam(R_SPREAD_PARAM, 0.f, 1.f, 0.f, "LvL R");
+		configParam(POLARITY_SWITCH, 0.f, 1.f, 0.f, "Polarity");
+		configParam(TIC_SELECTOR_SWITCH, 0.f, 1.f, 0.f, "Tic Selector");
 
-	void process(const ProcessArgs& args) override {
+		configInput(TIC1_INPUT, "Gate input");
+		configInput(TIC2_INPUT, "Gate input");
+		configInput(L_CV_INPUT, "CV L");
+		configInput(R_CV_INPUT, "CV R");
+
+		configOutput(OUT1_OUTPUT, "Left Output");
+		configOutput(OUT2_OUTPUT, "Right Output");
 	}
 };
-
 
 struct L_RanticsWidget : ModuleWidget {
 	L_RanticsWidget(L_Rantics* module) {
@@ -57,11 +63,11 @@ struct L_RanticsWidget : ModuleWidget {
 		addParam(createParamCentered<BefacoTinyKnob>(mm2px(Vec(13.571, 73.8)), module, L_Rantics::L_SPREAD_PARAM));
 		addParam(createParamCentered<BefacoTinyKnob>(mm2px(Vec(48.496, 73.8)), module, L_Rantics::R_SPREAD_PARAM));
 
-		addParam(createParam<CKSS>(mm2px(Vec(30.504, 49.33)), module, L_Rantics::BEAT_FRAC_INPUT));
-		addParam(createParam<CKSS>(mm2px(Vec(25.40, 33.30)), module, L_Rantics::SELECT_INPUT));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(30.504, 52.33)), module, L_Rantics::POLARITY_SWITCH));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(30.504, 35.30)), module, L_Rantics::TIC_SELECTOR_SWITCH));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(12.471, 35.84)), module, L_Rantics::CLOCK_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.391, 35.84)), module, L_Rantics::BPM_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(12.471, 35.84)), module, L_Rantics::TIC1_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.391, 35.84)), module, L_Rantics::TIC2_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(13.466, 87.3)), module, L_Rantics::L_CV_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.258, 87.3)), module, L_Rantics::R_CV_INPUT));
 
