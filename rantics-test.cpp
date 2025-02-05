0ab487ad0da7e54e0c471bb3a32cd0002c50066a
AgostoSound <agostosound@gmail.com>
2025-02-05 17:09:13 +0100

    Clean and add polarity draws.

--- rantics-test.cpp
+++ rantics-test.cpp
@@ -1,12 +1,20 @@
 #include "plugin.hpp"
+#include "random.hpp"
+#include <random>
+#include <chrono>
+#include <array>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include "componentlibrary.hpp"
 
 
 struct L_Rantics : Module {
 	enum ParamId {
 		L_SPREAD_PARAM,
 		R_SPREAD_PARAM,
-		POLARITY_PARAM,
-		TIC_SELECTOR_PARAM,
+		POLARITY_SWITCH,
+		TIC_SELECTOR_SWITCH,
 		PARAMS_LEN
 	};
 	enum InputId {
@@ -22,33 +30,25 @@ struct L_Rantics : Module {
 		OUTPUTS_LEN
 	};
 	enum LightId {
-		UNIPOLAR_LED_LIGHT,
-		BIPOLAR_LED_LIGHT,
 		LIGHTS_LEN
 	};
 
 	L_Rantics() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configParam(L_SPREAD_PARAM, 0.f, 1.f, 0.f, "LvL L");
+		configParam(R_SPREAD_PARAM, 0.f, 1.f, 0.f, "LvL R");
+		configParam(POLARITY_SWITCH, 0.f, 1.f, 0.f, "Polarity");
+		configParam(TIC_SELECTOR_SWITCH, 0.f, 1.f, 0.f, "Tic Selector");
 
-		configParam(L_SPREAD_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(R_SPREAD_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(POLARITY_PARAM, 0.f, 1.f, 0.f, "Polarity");
-		configParam(TIC_SELECTOR_PARAM, 0.f, 1.f, 0.f, "Tic Selector");
+		configInput(TIC1_INPUT, "Gate input");
+		configInput(TIC2_INPUT, "Gate input");
+		configInput(L_CV_INPUT, "CV L");
+		configInput(R_CV_INPUT, "CV R");
 
-		configInput(TIC1_INPUT, "");
-		configInput(TIC2_INPUT, "");
-		configInput(L_CV_INPUT, "");
-		configInput(R_CV_INPUT, "");
-
-		configOutput(OUT1_OUTPUT, "");
-		configOutput(OUT2_OUTPUT, "");
-	}
-
-	void process(const ProcessArgs& args) override {
-
+		configOutput(OUT1_OUTPUT, "Left Output");
+		configOutput(OUT2_OUTPUT, "Right Output");
 	}
 };
-
 
 struct L_RanticsWidget : ModuleWidget {
 	L_RanticsWidget(L_Rantics* module) {
@@ -63,23 +63,17 @@ struct L_RanticsWidget : ModuleWidget {
 		addParam(createParamCentered<BefacoTinyKnob>(mm2px(Vec(13.571, 73.8)), module, L_Rantics::L_SPREAD_PARAM));
 		addParam(createParamCentered<BefacoTinyKnob>(mm2px(Vec(48.496, 73.8)), module, L_Rantics::R_SPREAD_PARAM));
 
-		addParam(createParamCentered<CKSS>(mm2px(Vec(30.504, 52.33)), module, L_Rantics::POLARITY_PARAM));
-		addParam(createParamCentered<CKSS>(mm2px(Vec(30.504, 35.30)), module, L_Rantics::TIC_SELECTOR_PARAM));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(30.504, 52.33)), module, L_Rantics::POLARITY_SWITCH));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(30.504, 35.30)), module, L_Rantics::TIC_SELECTOR_SWITCH));
 
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(12.471, 35.84)), module, L_Rantics::TIC1_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.391, 35.84)), module, L_Rantics::TIC2_INPUT));
-		
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(13.466, 87.3)), module, L_Rantics::L_CV_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.258, 87.3)), module, L_Rantics::R_CV_INPUT));
 
 		addOutput(createOutputCentered<PJ3410Port>(mm2px(Vec(13.273, 106.29)), module, L_Rantics::OUT1_OUTPUT));
 		addOutput(createOutputCentered<PJ3410Port>(mm2px(Vec(48.383, 106.29)), module, L_Rantics::OUT2_OUTPUT));
-
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(25.742, 41.372)), module, L_Rantics::UNIPOLAR_LED_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(35.267, 41.372)), module, L_Rantics::BIPOLAR_LED_LIGHT));
-		
 	}
 };
-
 
 Model* modelL_Rantics = createModel<L_Rantics, L_RanticsWidget>("L-Rantics");
