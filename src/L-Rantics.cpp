26eba82b505d1adfa86fe0535e11bd134a9ce352
AgostoSound <agostosound@gmail.com>
2025-02-05 22:16:44 +0100

    Checkpoint front

--- src/L-Rantics.cpp
+++ src/L-Rantics.cpp
@@ -30,21 +30,28 @@ struct L_Rantics : Module {
 	L_Rantics() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 
-		configParam(L_SPREAD_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(R_SPREAD_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(L_SPREAD_PARAM, 0.f, 1.f, 0.f, "LvL L");
+		configParam(R_SPREAD_PARAM, 0.f, 1.f, 0.f, "LvL R");
+
 		configParam(POLARITY_PARAM, 0.f, 1.f, 0.f, "Polarity");
 		configParam(TIC_SELECTOR_PARAM, 0.f, 1.f, 0.f, "Tic Selector");
 
-		configInput(TIC1_INPUT, "");
-		configInput(TIC2_INPUT, "");
-		configInput(L_CV_INPUT, "");
-		configInput(R_CV_INPUT, "");
+		configInput(TIC1_INPUT, "Gate L");
+		configInput(TIC2_INPUT, "Gate R");
+		configInput(L_CV_INPUT, "LvL CV L");
+		configInput(R_CV_INPUT, "LvL CV R");
 
-		configOutput(OUT1_OUTPUT, "");
-		configOutput(OUT2_OUTPUT, "");
+		configOutput(OUT1_OUTPUT, "Random L");
+		configOutput(OUT2_OUTPUT, "Random R");
 	}
 
 	void process(const ProcessArgs& args) override {
+		// Obtiene el valor del parámetro de polaridad.
+		float polarity = params[POLARITY_PARAM].getValue();
+
+		// Encender el LED correspondiente.
+		lights[UNIPOLAR_LED_LIGHT].setBrightness(polarity == 0.f ? 1.f : 0.f);
+		lights[BIPOLAR_LED_LIGHT].setBrightness(polarity == 1.f ? 1.f : 0.f);
 
 	}
 };
@@ -69,14 +76,14 @@ struct L_RanticsWidget : ModuleWidget {
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(12.471, 35.84)), module, L_Rantics::TIC1_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.391, 35.84)), module, L_Rantics::TIC2_INPUT));
 		
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(13.466, 87.3)), module, L_Rantics::L_CV_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.258, 87.3)), module, L_Rantics::R_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(13.466, 87)), module, L_Rantics::L_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.258, 87)), module, L_Rantics::R_CV_INPUT));
 
 		addOutput(createOutputCentered<PJ3410Port>(mm2px(Vec(13.273, 106.29)), module, L_Rantics::OUT1_OUTPUT));
 		addOutput(createOutputCentered<PJ3410Port>(mm2px(Vec(48.383, 106.29)), module, L_Rantics::OUT2_OUTPUT));
 
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(25.742, 41.372)), module, L_Rantics::UNIPOLAR_LED_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(35.267, 41.372)), module, L_Rantics::BIPOLAR_LED_LIGHT));
+		addChild(createLightCentered<MediumLight<WhiteLight>>(mm2px(Vec(25.742, 52.33)), module, L_Rantics::UNIPOLAR_LED_LIGHT));
+		addChild(createLightCentered<MediumLight<WhiteLight>>(mm2px(Vec(35.267, 52.33)), module, L_Rantics::BIPOLAR_LED_LIGHT));
 		
 	}
 };
